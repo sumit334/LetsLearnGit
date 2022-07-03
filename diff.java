e7a89df3483bd130f81ffa9e20f80c175c4d5d98
Sumit Agarwal <agarwalsumit334@gmail.com>
2022-07-03 21:11:24 +0530

    Initial Value

---
+++ diff.java
@@ -0,0 +1,8 @@
+class diff{
+public static void main(String [] args){
+int a=5;
+int b=10;
+System.out.println("The difference of a and b is:" a-b);
+}
+}
+
--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ class sum{
 public static void main(String [] args){
 int a=5;
 int b=10;
-System.out.println(a+b);
+System.out.println("The sum of a and b is:" a+b);
 }
 }
 
